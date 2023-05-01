d39efc8db1ba370fe32450a569cebe067fed4219
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-05-02 00:34:33 +0300

    feat: add button & variables for Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,21 @@
-import React, {Component} from 'react';
-import logo from '../../images/logo-ntf.svg';
-import './header.scss';
+import React, { Component } from "react";
+import logo from "../../images/logo-ntf.svg";
+import "./header.scss";
 
 export default class Header extends Component {
-
     render() {
-        return(
-            <header className='header'>
-                <div className='heder__logo logo'>
-                    <img src={logo} alt='logo'/>
+        return (
+            <header className="header">
+                <div className="heder__logo logo">
+                    <img src={logo} alt="logo" />
                 </div>
-                <nav className='nav'>
-                    <ul className='nav__list'>
-                        <li className='nav__item'>Главная</li>
-                        <li className='nav__item'>Что даст обучение</li>
+                <nav className="nav">
+                    <ul className="nav__list">
+                        <li className="nav__item">Главная</li>
+                        <li className="nav__item">Что даст обучение</li>
                     </ul>
                 </nav>
-                <button>Личный кабинет</button>
+                <button className="header__button">Личный кабинет</button>
             </header>
         );
     }
