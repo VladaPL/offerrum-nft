db852e6c3ed5f7f335c04a2f40ec747d9c615815
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-05-02 16:35:05 +0300

    feat: add styles for header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,16 +6,20 @@ export default class Header extends Component {
     render() {
         return (
             <header className="header">
-                <div className="heder__logo logo">
-                    <img src={logo} alt="logo" />
+                <div className="wrapper">
+                    <div className="header__container">
+                        <div className="heder__logo logo">
+                            <img src={logo} alt="logo" />
+                        </div>
+                        <nav className="nav">
+                            <ul className="nav__list">
+                                <li className="nav__item">Главная</li>
+                                <li className="nav__item">Что даст обучение</li>
+                            </ul>
+                        </nav>
+                        <button className="header__button">Личный кабинет</button>
+                    </div>
                 </div>
-                <nav className="nav">
-                    <ul className="nav__list">
-                        <li className="nav__item">Главная</li>
-                        <li className="nav__item">Что даст обучение</li>
-                    </ul>
-                </nav>
-                <button className="header__button">Личный кабинет</button>
             </header>
         );
     }
