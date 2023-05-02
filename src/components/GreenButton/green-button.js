98561d26eaa5ae66da645c1763ca8c1066612176
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-05-02 13:51:56 +0300

    feat: - add GreenButton - add fonts - add slyles for Firstsection

---
+++ src/components/GreenButton/green-button.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./green-button.scss";
+
+export default class GreenButton extends Component {
+    render() {
+        const onClickHandler = this.props.onClick || (() => {});
+        const text = this.props.text || "";
+
+        return (
+            <button className="green-button" onClick={onClickHandler}>
+                {text}
+            </button>
+        );
+    }
+}
--- src/components/first-section/first-section.js
+++ src/components/first-section/first-section.js
@@ -1,17 +1,19 @@
 import "./first-section.scss";
 import manBig from "../../images/man-big.png";
+import GreenButton from "../GreenButton/green-button";
 
 const FirstSection = () => {
     return (
         <section className="first-section">
             <div className="first-section__left-block first-section__block">
                 <p className="first-section__top-text">
-                    Не упусти возможность войти в прибыльную нишу
+                    Не упусти возможность войти в <b>прибыльную нишу</b>
                 </p>
                 <p className="first-section__bottom-text">
                     Получи все нужные навыки для заработка на <h1>NFT</h1> всего
                     за 28 дней!
                 </p>
+                <GreenButton text="Начать зарабатывать на NFT" />
             </div>
             <div className="first-section__right-block first-section__block">
                 <img className="first-section__image" src={manBig} alt="man" />
