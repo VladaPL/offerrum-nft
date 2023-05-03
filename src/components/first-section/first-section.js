d15492248f9de5b115ef7577f42625eb7af1c748
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-05-03 10:35:52 +0300

    feat: finish styles for FirstSection desktop

--- src/components/first-section/first-section.js
+++ src/components/first-section/first-section.js
@@ -5,18 +5,20 @@ import GreenButton from "../GreenButton/green-button";
 const FirstSection = () => {
     return (
         <section className="first-section">
-            <div className="first-section__left-block first-section__block">
-                <p className="first-section__top-text">
-                    Не упусти возможность войти в <b>прибыльную нишу</b>
-                </p>
-                <p className="first-section__bottom-text">
-                    Получи все нужные навыки для заработка на <h1>NFT</h1> всего
-                    за 28 дней!
-                </p>
-                <GreenButton text="Начать зарабатывать на NFT" />
-            </div>
-            <div className="first-section__right-block first-section__block">
-                <img className="first-section__image" src={manBig} alt="man" />
+            <div className="wrapper">
+            <div className="first-section__right-block">
+                    <img className="first-section__image" src={manBig} alt="man" />
+                </div>
+                <div className="first-section__left-block first-section__block">
+                    <p className="first-section__top-text">
+                        Не упусти<br/> возможность войти<br/> в <b>прибыльную нишу</b>
+                    </p>
+                    <p className="first-section__bottom-text">
+                        Получи все нужные навыки для заработка<br/> на NFT всего
+                        за 28 дней!
+                    </p>
+                    <GreenButton text="Начать зарабатывать на NFT" />
+                </div>
             </div>
         </section>
     );
