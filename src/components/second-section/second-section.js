dafaa1ef14547b7376f299455e6283784c2c4d5c
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-05-03 14:00:08 +0300

    feat: add desktop styles for SecondSection

--- src/components/second-section/second-section.js
+++ src/components/second-section/second-section.js
@@ -1,4 +1,5 @@
 import "./second-section.scss";
+import "./decor.scss";
 
 import emojiDollars from '../../images/emoji-dollars.png';
 import emojiGlasses from '../../images/emoji-glasses.png';
@@ -8,37 +9,45 @@ import emojiTongua from '../../images/emoji-tongue.png';
 const arrayEmojiImages = [emojiStars, emojiTongua, emojiDollars, emojiGlasses];
 
 const SecondSection = () => {
+
     return (
         <section className="second-section">
-            <div className="second-section__center-block">
-                <h2>Что даст тебе обучение?</h2>
-                <div className="center-block__item">
-                    <div className="item__container">
-                        <img className="center-block__image" src={arrayEmojiImages[0]} alt="emoji"/>
-                        <p>Откроешь свой первый криптокошелек и научишься с ним работать</p>
+            <div className="wrapper">
+                <div className="second-section__center-block">
+                    <div className="ball-blue decor"></div>
+                    <div className="ball-normal decor"></div>
+                    <div className="ball-shadow decor"></div>
+                    <h2 className="second-section__title">Что даст тебе обучение?</h2>
+                    <div className="second-section__grid">
+                        <div className="grid__item">
+                            <div className="item__container">
+                                <img className="center-block__image" src={arrayEmojiImages[0]} alt="emoji"/>
+                                <div>Откроешь свой первый криптокошелек и научишься с ним работать</div>
+                            </div>
+                            <div className="item__container-bottom"></div>
+                        </div>
+                        <div className="grid__item">
+                            <div className="item__container">
+                                <img className="center-block__image" src={arrayEmojiImages[1]} alt="emoji"/>
+                                <div>Поймёшь, как выбирать правильные дропы</div>
+                            </div>
+                            <div className="item__container-bottom"></div>
+                        </div>
+                        <div className="grid__item">
+                            <div className="item__container">
+                                <img className="center-block__image" src={arrayEmojiImages[2]} alt="emoji"/>
+                                <div>Построишь план по быстрому приумножению заработанных средств</div>
+                            </div>
+                            <div className="item__container-bottom"></div>
+                        </div>
+                        <div className="grid__item">
+                            <div className="item__container">
+                                <img className="center-block__image" src={arrayEmojiImages[3]} alt="emoji"/>
+                                <div>Узнаешь кто такие холдеры и флипперы</div>
+                            </div>
+                            <div className="item__container-bottom"></div>
+                        </div>
                     </div>
-                    <div className="item__container-bottom"></div>
-                </div>
-                <div className="center-block__item">
-                    <div className="item__container">
-                        <img className="center-block__image" src={arrayEmojiImages[1]} alt="emoji"/>
-                        <p>Поймёшь, как выбирать правильные дропы</p>
-                    </div>
-                    <div className="item__container-bottom"></div>
-                </div>
-                <div className="center-block__item">
-                    <div className="item__container">
-                        <img className="center-block__image" src={arrayEmojiImages[2]} alt="emoji"/>
-                        <p>Построишь план по быстрому приумножению заработанных средств</p>
-                    </div>
-                    <div className="item__container-bottom"></div>
-                </div>
-                <div className="center-block__item">
-                    <div className="item__container">
-                        <img className="center-block__image" src={arrayEmojiImages[3]} alt="emoji"/>
-                        <p>Узнаешь кто такие холдеры и флипперы</p>
-                    </div>
-                    <div className="item__container-bottom"></div>
                 </div>
             </div>
         </section>
