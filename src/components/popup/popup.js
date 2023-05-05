10fbdbd2f5e9572e424c9bdf9acb9f53d5c9407b
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-05-05 13:01:31 +0300

    feat: add popup background & button-close

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -5,21 +5,24 @@ import "./popup.scss";
 export default class Popup extends Component {
     render() {
         return (
-            <div className="popup">
-                <div className="backlight"></div>
-                <div className="popup__container">
-                    <h3 className="popup__title">Начни прямо сейчас!</h3>
-                    <p className="popup__text">
-                        Получи все нужные навыки для заработка<br/> на NFT всего
-                        за 28 дней!
-                    </p>
-                    <form className="popup__form" autoComplete="on">
-                        <input className="popup__input" type="email" name="email" placeholder="Ваш email"/>
-                        <div className="popup__button">
-                            <GreenButton text="Оплатить" />
-                        </div>
-                    </form>
-                </div>  
+            <div className="popup__background">
+                <div className="popup">
+                    <div className="backlight"></div>
+                    <div className="popup__close"></div>
+                    <div className="popup__container">
+                        <h3 className="popup__title">Начни прямо сейчас!</h3>
+                        <p className="popup__text">
+                            Получи все нужные навыки для заработка<br/> на NFT всего
+                            за 28 дней!
+                        </p>
+                        <form className="popup__form" autoComplete="on">
+                            <input className="popup__input" type="email" name="email" placeholder="Ваш email"/>
+                            <div className="popup__button">
+                                <GreenButton text="Оплатить" />
+                            </div>
+                        </form>
+                    </div>  
+                </div>
             </div>
         );
     }
